2789c77cc4205abcd51524bea9efa3d3a8607a5f
Luiswicho94 <luiscesar94@hotmail.com>
2021-03-22 12:51:35 -0700

    Initial commit of file plot3.R and resulting graph stored in plot3.png

---
+++ plot3.R
@@ -0,0 +1,20 @@
+## ##Reading table, labeling and subsetting February data. (As used in Plot1.R)
+Power <- read.table("household_power_consumption.txt", skip = 1, sep = ";")
+names(Power) <- c("Date", "Time", "Global_Active_Power", "Global_Reactive_Power", "Voltage", "Global_Intensity", "Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3")
+Subpower <- subset(Power, Power$Date=="1/2/2007" | Power$Date == "2/2/2007")
+
+##Reclass Date and Time variables to objects. (As used in plot2.R)
+Subpower$Date <- as.Date(Subpower$Date, format = "%d/%m/%Y")
+Subpower$Time <- strptime(Subpower$Time, format = "%H:%M:%S")
+Subpower[1:1440, "Time"] <- format(Subpower[1:1440, "Time"], "2007-02-01 %H:%M:%S")
+Subpower[1441:2880, "Time"] <- format(Subpower[1441:2880, "Time"], "2007-02-02 %H:%M:%S")
+
+##PNG and various plotting on same graph.
+png("plot3.png", width = 480, height = 480)
+plot(Subpower$Time, Subpower$Sub_Metering_1, type= "n", xlab = "", ylab="Energy Sub Metering")
+with(Subpower, lines(Time, as.numeric(as.character(Sub_Metering_1))))
+with(Subpower, lines(Time, as.numeric(as.character(Sub_Metering_2)), col = "red"))
+with(Subpower, lines(Time, as.numeric(as.character(Sub_Metering_3)), col = "blue"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"))
+title(main= "Energy Sub Metering")
+dev.off()
